bc846f6017e6f350744bdef27a653ab8ef5d215c
tawfek <tawfek12121@gmail.com>
2019-09-03 10:32:04 +0200

    tawfek

---
+++ js/first.js
@@ -0,0 +1,42 @@
+console.log(
+    "mein alter ist 26 " +
+    (
+        36 +
+        37 +
+        27 +
+        26 +
+        32 +
+        29 +
+        30 +
+        26 +
+        34 +
+        24 +
+        30 +
+        31 +
+        30 +
+        32 +
+        30 +
+        23
+          )
+          /16
+          )
+var firstname="Tawfek"
+var lastname="Zahed"
+var fullName=firstname +" "+ lastname
+var age= 2019 - 1993
+console.log(fullName+ " " + age)
+
+//var Gewicht Mark
+//var Größe Mark
+
+//var Gewicht John
+//var Größe John
+
+//var BMI Mark = Gewicht / (Gröse * Gröse)
+//var BMI John = Gewicht / (Gröse * Gröse)
+
+//var Mark hat höheren BMI [Boolean]
+
+//Ausgabe: "Hat Mark einen höheren BMI? " + Ergebnis Vergleich
+
+
